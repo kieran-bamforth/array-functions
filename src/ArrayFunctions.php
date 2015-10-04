a23f3ba0c026325c4bd4dca2a0d64997e39e6824
Kieran Bamforth <hello@kieranbamforth.me>
2015-10-04 17:28:30 +0100

    Added empty ArrayFunctions class.

---
+++ src/ArrayFunctions.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace KieranBamforth\ArrayFunctions;
+
+class ArrayFunctions
+{
+    /**
+     * Flattens all of a multidimensional array's values into a single array.
+     *
+     * @param  array $input The multidimensional array to flatten.
+     *
+     * @return array
+     */
+    public function flatten(array $input)
+    {
+        $flattenedArray = [];
+
+        return $flattenedArray;
+    }
+}
